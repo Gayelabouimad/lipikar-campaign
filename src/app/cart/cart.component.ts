ff642cfc72f3fc3b91566ad29f75ceef5242c72f
gabouimad <gabouimad@murex.com>
2024-12-04 20:27:04 +0200

    should squash

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, FormsModule ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  shippingFrequency: number = 1;
 
+  increaseQuantity() {
+    this.shippingFrequency++;
+  }
+
+  decreaseQuantity() {
+    if (this.shippingFrequency > 1) {
+      this.shippingFrequency--;
+    }
+  }
 }
