de300cc289b4c4ff6be7987ed648d9510209287b
gabouimad <gabouimad@murex.com>
2024-12-04 19:39:23 +0200

    build3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CartComponent } from './cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainPageComponent }, // Default route
+    { path: 'cart', component: CartComponent }, // Cart route accessible only by URL
+    { path: '**', redirectTo: '' } // Redirect unknown paths to the main page
+];
